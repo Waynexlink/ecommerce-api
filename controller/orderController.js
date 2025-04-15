7527526d50691edf4d41e17a9b4aac2f70715e39
Waynexlink <wm500972@gmail.com>
2025-04-15 14:23:30 +0100

    fixed orders

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,16 +1,16 @@
 const Cart = require("../models/cartModel");
 const Order = require("../models/orderModel");
-const Product = require("../models/orderModel");
+const Product = require("../models/productModel");
 
-const createOrderFromCart = async (req, res, next) => {
-  const userId = req.user.Id;
+const createOrderFromCart = async (req, res) => {
+  const userId = req.user._id;
 
   try {
-    const cart = Cart.findOne({ userId }).populate({
+    const cart = await Cart.findOne({ userId }).populate({
       path: "items.productId",
       select: "name price",
     });
-    if (!cart || cart.length === 0) {
+    if (!cart || cart.items.length === 0) {
       return res.status(400).json({
         status: "fail",
         message: "Cannot create order for empty cart",
@@ -18,7 +18,7 @@ const createOrderFromCart = async (req, res, next) => {
     }
     let totalPrice = 0;
     let orderItem = cart.items.map((item) => {
-      if (item.productId || typeof item.productId === undefined) {
+      if (item.productId || typeof item.productId.price === undefined) {
         throw new Error(
           `product details missing for items id:${item.productId?._id}`
         );
@@ -33,7 +33,7 @@ const createOrderFromCart = async (req, res, next) => {
       };
     });
 
-    const newOrder = Order.create({
+    const newOrder = await Order.create({
       userId,
       orderItem,
       totalPrice,
@@ -50,19 +50,23 @@ const createOrderFromCart = async (req, res, next) => {
     console.log(`Error creating order from cart: ${error} `);
     res.status(500).json({
       status: "error",
-      message: error.message,"Internal server error creating order";
+      message: error.message || "Internal server error creating order",
     });
   }
 };
-const viewMyOrders = (req, res, next) => {
+const viewMyOrders = async (req, res) => {
+  const userId = req.user._id;
   try {
-    //add pagination and filtering with apifeatures class so i can use in multiple places
-    const orders = Order.find();
+    const orders = await Order.find({ userId }).populate({
+      path: "items.productId",
+      select: "name price",
+    });
 
-    if (!orders || orders === null) {
+    if (!orders) {
       return res.status(200).json({
         status: "success",
         message: "No orders yet",
+        orders: [],
       });
     }
     res.status(200).json({
@@ -70,33 +74,99 @@ const viewMyOrders = (req, res, next) => {
       message: "orders fetched successfully",
       orders,
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(`Error fetching orders: ${error} `);
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error creating order",
+    });
+  }
 };
 
-const viewOrder = (req, res, next) => {
-  const { userId } = req.params;
+const viewOrder = async (req, res) => {
+  const userId = req.user._id;
+  const { orderId } = req.params;
   try {
-    const order = Order.findOne({ userId });
+    const order = await Order.findOne({ _id: orderId, userId }).populate({
+      path: "items.productId",
+      select: "name price",
+    });
     if (!order)
       return res.status(404).message({
         status: "fail",
-        message: "This order does not exist",
+        message: "This order does not exist or forbidden",
       });
     res.status(200).json({
       status: "success",
       message: "order retrived successfully",
       order,
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(`Error fetching order: ${error} `);
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error creating order",
+    });
+  }
 };
+//admin operations
+const viewAllOrders = (req, res) => {
+  try {
+    //restricted to just admin
+    const order = Order.find();
+    if (!order.length)
+      return res.status(404).json({
+        status: "fail",
+        message: "No order found",
+        order: [],
+      });
 
-const viewAllOrders = (req, res) => {};
+    res.status(200).json({
+      status: "success",
+      message: "Orders retrived successfully",
+      order,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error retriving orders",
+    });
+  }
+};
 const updateOrder = (req, res) => {
-  const { productId } = req.params;
+  const { orderId } = req.params;
   const { orderStatus } = req.body;
 
   try {
-  } catch (error) {}
+    const validStatus = ["processing", "shipped", "delivered"];
+    if (!validStatus.includes(orderStatus)) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Status not valid",
+      });
+    }
+    const updatedOrder = Order.findByIdAndUpdate(
+      orderId,
+      { orderStatus },
+      { new: true, runValidators: true }
+    );
+    if (!updatedOrder)
+      return res.status(404).json({
+        status: "fail",
+        message: "Order not found",
+      });
+
+    res.status(200).json({
+      status: "success",
+      message: "Order status updated",
+      order: updatedOrder,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error updating orders",
+    });
+  }
 };
 
 module.exports = {
