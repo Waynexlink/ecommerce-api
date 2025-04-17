976ddef77dc3751d806c56f5ea2b828a42be7378
Waynexlink <wm500972@gmail.com>
2025-04-17 11:12:26 +0100

    added reset password

--- controller/authController.js
+++ controller/authController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/AppError");
+const sendEmail = require("../utils/emailSender");
 //signup cpontroller
 const signupUser = catchAsync(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -47,6 +48,89 @@ const loginUser = catchAsync(async (req, res, next) => {
     token,
   });
 });
-const forgotPassword = catchAsync(async (req, res, next) => {});
-const resetPassword = catchAsync(async (req, res, next) => {});
+const forgotPassword = catchAsync(async (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email) return next(new AppError("Please provide your email", 400));
+
+  const user = User.findOne({ email });
+
+  res.status(200).json({
+    status: "success",
+    message:
+      "If a user with that email exists, a password reset link has been sent.",
+  });
+
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  const resetURL = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
+  const message = `Seems like you forgot your password ,if this is true ,click on the link below to reset your password\n ${resetURL}\n\nIf you didn’t request this, please ignore this email.`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Reset your password",
+      message,
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Token sent to email!",
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError(
+        "There was an error sending the email. Try again later.",
+        500
+      )
+    );
+  }
+});
+const resetPassword = catchAsync(async (req, res, next) => {
+  const { password, passwordConfirm } = req.body;
+
+  if (!password || !passwordConfirm) {
+    return next(
+      new AppError("Please provide and confirm your new password.", 400)
+    );
+  }
+
+  if (password !== passwordConfirm) {
+    return next(new AppError("Passwords do not match.", 400));
+  }
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(req.params.token)
+    .digest("hex");
+
+  const user = await User.find({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(new AppError("Token is invalid or has expired.", 400));
+  }
+
+  user.password = password;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+
+  await user.save();
+  const payload = { id: user._id, role: user.role };
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: "1D",
+  });
+  res.status(200).json({
+    status: "success",
+    message: "Password successfully reset.",
+    token,
+  });
+});
 module.exports = { signupUser, loginUser, forgotPassword, resetPassword };
