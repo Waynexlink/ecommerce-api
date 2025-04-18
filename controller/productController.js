0b34af445c1fef6bd15331bc5d933bbfec695050
Waynexlink <wm500972@gmail.com>
2025-04-18 01:45:58 +0100

    more product routes

--- controller/productController.js
+++ controller/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/AppError");
+const { findByIdAndDelete } = require("../models/userModel");
 
 const getAllProducts = catchAsync(async (req, res, next) => {
   const products = await Product.find({});
@@ -39,4 +40,88 @@ const createProduct = catchAsync(async (req, res, next) => {
     product,
   });
 });
-module.exports = { getAllProducts, getProductById, createProduct };
+
+const updateProduct = catchAsync(async (req, res, next) => {
+  const { productId } = req.params;
+  const allowedUpdates = [
+    "name",
+    "description",
+    "price",
+    "quantity",
+    "image",
+    "category",
+  ];
+
+  const updateData = {};
+
+  allowedUpdates.forEach((update) => {
+    if (req.body[update] !== undefined) {
+      updateData[update] = req.body[update];
+    }
+  });
+
+  const product = Product.findByIdAndUpdate(productId, updateData, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!product) return next(new AppError("Product not found", 404));
+
+  res.status(200).json({
+    status: "success",
+    message: "Product successfully updated",
+    product,
+  });
+});
+const deleteProduct = catchAsync(async (req, res, next) => {
+  const { productId } = req.params;
+
+  const product = await Product.findByIdAndDelete(productId);
+  if (!product) return next(new AppError("Product not found", 404));
+
+  res.status(200).json({
+    status: "success",
+    message: "Product deleted successfully",
+  });
+});
+const viewProductInventory = catchAsync(async (req, res, next) => {
+  const products = await Product.find().select("name quantity");
+
+  if (!products || products.length == 0)
+    return next(new AppError("No product found in this Inventory", 404));
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      products,
+    },
+  });
+});
+const updateQuantity = catchAsync(async (req, res, next) => {
+  const { productId } = req.params;
+  const { quantity } = req.body;
+
+  const product = await Product.findByIdAndUpdate(
+    productId,
+    { quantity },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  if (!product) return next(new AppError("Product not found", 404));
+
+  res.status(200).json({
+    status: "success",
+    message: "Quantity updated successfully",
+  });
+});
+module.exports = {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  viewProductInventory,
+  updateQuantity,
+};
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,12 +4,27 @@ const {
   getAllProducts,
   getProductById,
   createProduct,
+  updateProduct,
+  deleteProduct,
+  viewProductInventory,
+  updateQuantity,
 } = require("../controller/productController");
 const { requireAuth, requireAdmin } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/", getAllProducts);
 router.get("/:productId", getProductById);
+//admin functionality
+
 router.post("/", requireAuth, requireAdmin, createProduct);
+router.put("/:productId", requireAuth, requireAdmin, updateProduct);
+router.delete("/:productId", requireAuth, requireAdmin, deleteProduct);
+router.get(
+  "/:productId/inventory",
+  requireAuth,
+  requireAdmin,
+  viewProductInventory
+);
+router.put("/:productId/inventory", requireAuth, requireAdmin, updateQuantity);
 
 module.exports = router;
